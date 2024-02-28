316ccf5d7cb404aec33ec0da8dee19a4a322bcd7
sickmantella <missilescout@gmail.com>
2024-02-27 21:25:19 -0700

    Needs more PID tuning

--- src/main/java/frc/robot/commands/DriveAndPickupNoteCmd.java
+++ src/main/java/frc/robot/commands/DriveAndPickupNoteCmd.java
@@ -25,20 +25,19 @@ public class DriveAndPickupNoteCmd extends Command {
   private double tv, tx, ty, ta, lastYaw, lateralDistanceToNote, distanceFromNote, predictedLateralDistanceTraveled;
   private double degreesToRotate;
   private double tOld, tNew;
-  private boolean detectNotes, moveToNote;
+  private boolean detectNotes, moveToNote, seesNote;
 
   // Change these values as needed
-  private double noteSizeThreshold = 0.5;                 // Minimum size of the NOTE for the robot to move to and pickup.
+  private double noteSizeThreshold = 0.3;                 // Minimum size of the NOTE for the robot to move to and pickup.
   private double overshootDistance = 0.5;                 // This is how many meters will be added to the distance to try to run over the NOTE.
-  private double LimeLightHeight = 0.71;                  // The LimeLight distance off the floor in meters, used to estimate distance to a NOTE.
+  private double LimeLightHeight = 0.9806;                // The LimeLight distance off the floor in meters, used to estimate distance to a NOTE.
   private double minimumNoteAngle = -21.5;                // The minimum angle for the LimeLight to calculate distance to the NOTE. Below this, the robot will estimate.
-  private String limelighNetworkTableName = VisionConstants.kPickupLimelightNetworkTableName;
-  private double translationP = 0.05;                      // P can be 0.05 after initial testing
+  private double translationP = 0.38;                     // P can be 0.05 after initial testing
   private double translationI = 0;
   private double translationD = 0;
 
-  private double rotationP = 0.004;
-  private double rotationI = 0.002;
+  private double rotationP = 0.0075;
+  private double rotationI = 0;
   private double rotationD = 0.0005;
   //
 
@@ -56,7 +55,8 @@ public class DriveAndPickupNoteCmd extends Command {
     this.longitudinalPidController = new PIDController(translationP, translationI, translationD);
     this.rotationalPidController = new PIDController(rotationP, rotationI, rotationD);
 
-    this.limelighNetworkTable = NetworkTableInstance.getDefault().getTable(limelighNetworkTableName);
+    // this.limelighNetworkTable = NetworkTableInstance.getDefault().getTable(VisionConstants.kPickupLimelightNetworkTableName);
+    this.limelighNetworkTable = NetworkTableInstance.getDefault().getTable(VisionConstants.kShootLimelightNetworkTableName);
 
     lateralPidController.setSetpoint(0);
     longitudinalPidController.setSetpoint(0);
@@ -70,12 +70,11 @@ public class DriveAndPickupNoteCmd extends Command {
   public void initialize() {
     previousPipeline = limelighNetworkTable.getEntry("pipeline").getDouble(0);
     limelighNetworkTable.getEntry("pipeline").setNumber(1);
-    SmartDashboard.putBoolean("DriveAndPickupNote", true);
     tv = 0;
     tx = 0;
     ty = 0;
     ta = 0;
-    degreesToRotate = 360; // Rotate in a full circle to find any NOTES.
+    degreesToRotate = 0;
     detectNotes = true; // Try to search for NOTES
     moveToNote = false; // Rotate to find NOTES, don't immeaditly try to move to one and pick it up.
     lastYaw = m_swerveSubsystem.getChassisYaw();
@@ -83,6 +82,7 @@ public class DriveAndPickupNoteCmd extends Command {
     predictedLateralDistanceTraveled = 0;
     tOld = 0;
     tNew = 0;
+    seesNote = false;
   }
 
   // Called every time the scheduler runs while the command is scheduled.
@@ -90,6 +90,7 @@ public class DriveAndPickupNoteCmd extends Command {
   public void execute() {
     // Initialize ChassisSpeeds to control robot movement
     ChassisSpeeds chassisSpeeds;
+    chassisSpeeds = new ChassisSpeeds(0,0,0);
 
     // If the robot should look for a NOTE, get data from the LimeLight and rotate in place.
     if(detectNotes == true) {
@@ -99,11 +100,25 @@ public class DriveAndPickupNoteCmd extends Command {
       ty = limelighNetworkTable.getEntry("ty").getDouble(0);
       ta = limelighNetworkTable.getEntry("ta").getDouble(0);
 
+      if(tv != 0) {
+        seesNote = true;
+      }
       // Decrease degreesToRotate as the robot completes its search.
       degreesToRotate = degreesToRotate - (lastYaw - m_swerveSubsystem.getChassisYaw());
       lastYaw = m_swerveSubsystem.getPose().getRotation().getDegrees();
-      chassisSpeeds = new ChassisSpeeds(0, 0, rotationalPidController.calculate(degreesToRotate));
-
+      if(!moveToNote) {
+        chassisSpeeds = new ChassisSpeeds(0, 0, rotationalPidController.calculate(degreesToRotate));
+      }
+      if(moveToNote) {
+      tOld = tNew;
+      tNew = System.nanoTime() / Math.pow(10, 9);
+      // Predict distance traveled based on meters/second multiplied by time traveling at the velocity.
+      if(tOld != 0 && tNew != 0) {
+        predictedLateralDistanceTraveled = predictedLateralDistanceTraveled + (m_swerveSubsystem.getRobotRelativeSpeeds().vxMetersPerSecond * (tNew - tOld));
+        lateralDistanceToNote = distanceFromNote + overshootDistance - predictedLateralDistanceTraveled;
+        chassisSpeeds = new ChassisSpeeds(longitudinalPidController.calculate(-lateralDistanceToNote), lateralPidController.calculate(tx), rotationalPidController.calculate(degreesToRotate));
+      }
+      }
       // If the robot sees a NOTE that is close and isn't pointed at it, stop the full-circle scan and point the robot at the NOTE.
       if(ta >= noteSizeThreshold && Math.abs(tx) > 5) {
         degreesToRotate = tx;
@@ -113,7 +128,7 @@ public class DriveAndPickupNoteCmd extends Command {
       if(ta >= noteSizeThreshold && Math.abs(tx) <= 5) {
         // Guess the distance until the robot encounters the NOTE. Begin moving towards the NOTE.
         if((lateralDistanceToNote == 0 || ty >= minimumNoteAngle) && !moveToNote) {
-          distanceFromNote = Math.tan((90 - Math.abs(ty)) * Math.PI/180) * LimeLightHeight;
+          distanceFromNote = Math.tan((90 - Math.abs(ty) - 17) * Math.PI/180) * LimeLightHeight;
           moveToNote = true;
         }
 
@@ -124,47 +139,45 @@ public class DriveAndPickupNoteCmd extends Command {
         }
       }
     } 
-    else {
+    if(detectNotes == false) {
       tv = 0;
       tx = 0;
       ty = 0;
       ta = 0;
-    chassisSpeeds = new ChassisSpeeds(0, 0, 0);
+      chassisSpeeds = new ChassisSpeeds(0, 0, 0);
     }
 
     // If a NOTE is seen, go there and record timestamps to calculate distance traveled.
-    if(moveToNote == true) {        
+    if(detectNotes == false && moveToNote == true) {        
       tOld = tNew;
       tNew = System.nanoTime() / Math.pow(10, 9);
-      SmartDashboard.putNumber("tnew", tNew);
       // Predict distance traveled based on meters/second multiplied by time traveling at the velocity.
       if(tOld != 0 && tNew != 0) {
         predictedLateralDistanceTraveled = predictedLateralDistanceTraveled + (m_swerveSubsystem.getRobotRelativeSpeeds().vxMetersPerSecond * (tNew - tOld));
         lateralDistanceToNote = distanceFromNote + overshootDistance - predictedLateralDistanceTraveled;
-        SmartDashboard.putNumber("predictedLateralDistanceTraveled", predictedLateralDistanceTraveled);
-        chassisSpeeds = new ChassisSpeeds(longitudinalPidController.calculate(-lateralDistanceToNote), lateralPidController.calculate(tx), 0);
+        chassisSpeeds = new ChassisSpeeds(longitudinalPidController.calculate(-lateralDistanceToNote), lateralPidController.calculate(tx), rotationalPidController.calculate(degreesToRotate));
       }
     }
-
+    if(!seesNote) {
+      chassisSpeeds = new ChassisSpeeds(0,0,0);
+    }
     // Convert chassis speeds to individual module states
     SwerveModuleState[] moduleStates = DriveConstants.kDriveKinematics.toSwerveModuleStates(chassisSpeeds);
     
     // Output each module states to wheels
     m_swerveSubsystem.setModuleStates(moduleStates);
-    SmartDashboard.putNumber("lateralDistanceToNote", lateralDistanceToNote);
-    SmartDashboard.putNumber("distanceFromNote", distanceFromNote);
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
     //limelighNetworkTable.getEntry("pipeline").setNumber(previousPipeline);
-    SmartDashboard.putBoolean("DriveAndPickupNote", false);
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
+    SmartDashboard.putNumber("lateralDistanceToNote", lateralDistanceToNote);
     if(Math.abs(lateralDistanceToNote) <= 0.15 && Math.abs(lateralDistanceToNote) > 0) {  // Add or statement for NOTE in pickup
         return true;
     }
